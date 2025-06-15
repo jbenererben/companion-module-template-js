04603917af4b15d9e6fd75129b497d7169080845
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-15 15:34:56 +0300

    Update action.js

--- companion/action.js
+++ companion/action.js
@@ -1,66 +1,89 @@
 module.exports = function (self) {
-	self.setActionDefinitions({
-		// 1. Action: PVW Layerlarını Konsola Yazdır
-		get_pvw_layers: {
-			name: 'Seçili Screen’in PVW Layerlarını Çek',
-			options: [
-				{
-					id: 'screen_id',
-					type: 'number',
-					label: 'Screen ID',
-					default: 1,
-					min: 1,
-				},
-			],
-			callback: async (event) => {
-				const ip = self.config.ip || '127.0.0.1'
-				const screenId = event.options.screen_id
-				const url = `http://${ip}:19998/unico/v1/layers/list-detail`
+  self.setActionDefinitions({
+    // 1. Seçili screen’in PVW layer’larını konsola yazdır
+    get_pvw_layers: {
+      name: 'Seçili Screen’in PVW Layerlarını Çek',
+      options: [
+        {
+          id: 'screen_id',
+          type: 'number',
+          label: 'Screen ID',
+          default: 1,
+          min: 1,
+        },
+      ],
+      callback: async (event) => {
+        const ip = self.config.host || self.config.ip || '127.0.0.1'
+        const screenId = event.options.screen_id
+        const url = `http://${ip}:19998/unico/v1/layers/list-detail`
 
-				try {
-					const res = await self.system.emit('http_get', url, {})
-					// Response içindeki layerlardan, ilgili screen’e ait ve PVW olanları buluyoruz
-					if (res.data && res.data.data && res.data.data.list) {
-						const pvwLayers = res.data.data.list.filter(l =>
-							l.layerIdObj && l.layerIdObj.attachScreenId === screenId &&
-							l.UMD && l.UMD.some(u => (u.name || '').toUpperCase().includes('PVW'))
-						)
-						console.log('Seçilen Screen’in PVW Layerları:', pvwLayers)
-					} else {
-						console.log('Veri bulunamadı')
-					}
-				} catch (err) {
-					console.log('HTTP isteğinde hata:', err)
-				}
-			},
-		},
+        try {
+          const res = await self.system.emit('http_get', url, {})
+          if (res.data && res.data.data && Array.isArray(res.data.data.list)) {
+            const pvwLayers = res.data.data.list.filter(l =>
+              l.layerIdObj && l.layerIdObj.attachScreenId === screenId &&
+              l.UMD && l.UMD.some(u => (u.name || '').toUpperCase().includes('PVW'))
+            )
+            console.log('Seçilen Screen’in PVW Layerları:', pvwLayers)
+          } else {
+            console.log('Veri bulunamadı')
+          }
+        } catch (err) {
+          console.log('HTTP isteğinde hata:', err)
+        }
+      },
+    },
 
-		// 2. Action: PVW Layer Seçtir
-		select_pvw_layer: {
-			name: 'PVW Layer Seç',
-			options: [
-				{
-					id: 'screen_id',
-					type: 'number',
-					label: 'Screen ID',
-					default: 1,
-					min: 1,
-				},
-				{
-					id: 'layer_id',
-					type: 'textinput',
-					label: 'Layer ID',
-					default: '',
-				},
-			],
-			callback: async (event) => {
-				// Burada da layer seçme ve/veya input atama işlemlerini yapacağız
-				const ip = self.config.ip || '127.0.0.1'
-				const screenId = event.options.screen_id
-				const layerId = event.options.layer_id
-				console.log(`Seçilen Screen: ${screenId}, Seçilen Layer: ${layerId}`)
-				// Buraya ileride layerı aktive edecek veya input atayacak kodu ekleyeceğiz!
-			},
-		}
-	})
-},
+    // 2. Serial ile PVW Layer bulucu (ör. L1/L2/L3…)
+    get_pvw_layer_by_serial: {
+      name: 'PVW Layerı Serial ile Seç',
+      options: [
+        {
+          id: 'screen_id',
+          type: 'number',
+          label: 'Screen ID',
+          default: 1,
+          min: 1,
+        },
+        {
+          id: 'serial',
+          type: 'number',
+          label: 'Layer Serial (L1 için 1, L2 için 2, vb)',
+          default: 1,
+          min: 1,
+        },
+      ],
+      callback: async (event) => {
+        const ip = self.config.host || self.config.ip || '127.0.0.1'
+        const screenId = event.options.screen_id
+        const serial = event.options.serial
+        const url = `http://${ip}:19998/unico/v1/layers/list-detail`
+
+        try {
+          const res = await self.system.emit('http_get', url, {})
+          if (res.data && res.data.data && Array.isArray(res.data.data.list)) {
+            const pvwLayers = res.data.data.list.filter(l =>
+              l.layerIdObj && l.layerIdObj.attachScreenId === screenId &&
+              l.UMD && l.UMD.some(u => (u.name || '').toUpperCase().includes('PVW'))
+            )
+            // Serial, bazen doğrudan, bazen l.general.serial içinde olabilir
+            const targetLayer = pvwLayers.find(
+              l => l.serial === serial ||
+              (l.general && l.general.serial === serial)
+            )
+            if (targetLayer) {
+              console.log(`Screen ${screenId} PVW'deki L${serial}:`, targetLayer)
+              // Burada layer ile yapılacak başka işleme ileride kod eklenebilir
+            } else {
+              console.log(`Screen ${screenId} PVW'de serial ${serial} bulunamadı.`)
+            }
+          } else {
+            console.log('Veri bulunamadı')
+          }
+        } catch (err) {
+          console.log('HTTP isteğinde hata:', err)
+        }
+      },
+    }
+  })
+}
