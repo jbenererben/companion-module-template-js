d3ad5f5306a547e15e300f479897d5bb415f793b
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-15 14:02:58 +0300

    Create config.js
    Create config.js and add IP address input

---
+++ companion/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  config: [
+    {
+      type: 'textinput',
+      id: 'host',
+      label: 'Device IP address',
+      width: 6,
+      default: '192.168.1.100'
+    }
+  ]
+}
