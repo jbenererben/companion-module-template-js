f80e8bad859d7e3ab1e632a0a38c08e2361a5ff9
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-15 14:49:34 +0300

    Update action.js

--- companion/action.js
+++ companion/action.js
@@ -1,6 +1,6 @@
 module.exports = function (self) {
 	self.setActionDefinitions({
-		// Diğer actionlar burada olabilir
+		// 1. Action: PVW Layerlarını Konsola Yazdır
 		get_pvw_layers: {
 			name: 'Seçili Screen’in PVW Layerlarını Çek',
 			options: [
@@ -13,16 +13,13 @@ module.exports = function (self) {
 				},
 			],
 			callback: async (event) => {
-				const ip = self.config.ip || '127.0.0.1';
-				const screenId = event.options.screen_id;
-				const url = `http://${ip}:19998/unico/v1/layers/list-detail`;
+				const ip = self.config.ip || '127.0.0.1'
+				const screenId = event.options.screen_id
+				const url = `http://${ip}:19998/unico/v1/layers/list-detail`
 
 				try {
-					const res = await self.system.emit('http_get', url, {});
-					// Eğer Companion 3'te context.http.get varsa onu da kullanabilirsin, örnek:
-					// const res = await context.http.get(url);
-
-					// Burada response içindeki layerlardan ilgili screen’e ait ve PVW olanları bulacaksın
+					const res = await self.system.emit('http_get', url, {})
+					// Response içindeki layerlardan, ilgili screen’e ait ve PVW olanları buluyoruz
 					if (res.data && res.data.data && res.data.data.list) {
 						const pvwLayers = res.data.data.list.filter(l =>
 							l.layerIdObj && l.layerIdObj.attachScreenId === screenId &&
@@ -30,12 +27,40 @@ module.exports = function (self) {
 						)
 						console.log('Seçilen Screen’in PVW Layerları:', pvwLayers)
 					} else {
-						console.log('Veri bulunamadı');
+						console.log('Veri bulunamadı')
 					}
 				} catch (err) {
-					console.log('HTTP isteğinde hata:', err);
+					console.log('HTTP isteğinde hata:', err)
 				}
 			},
 		},
+
+		// 2. Action: PVW Layer Seçtir
+		select_pvw_layer: {
+			name: 'PVW Layer Seç',
+			options: [
+				{
+					id: 'screen_id',
+					type: 'number',
+					label: 'Screen ID',
+					default: 1,
+					min: 1,
+				},
+				{
+					id: 'layer_id',
+					type: 'textinput',
+					label: 'Layer ID',
+					default: '',
+				},
+			],
+			callback: async (event) => {
+				// Burada da layer seçme ve/veya input atama işlemlerini yapacağız
+				const ip = self.config.ip || '127.0.0.1'
+				const screenId = event.options.screen_id
+				const layerId = event.options.layer_id
+				console.log(`Seçilen Screen: ${screenId}, Seçilen Layer: ${layerId}`)
+				// Buraya ileride layerı aktive edecek veya input atayacak kodu ekleyeceğiz!
+			},
+		}
 	})
-}
+},
