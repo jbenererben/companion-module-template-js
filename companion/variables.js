357c3feadc5026a8335c5772d31c989c378b2c21
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-15 16:16:47 +0300

    Update variables.js

--- companion/variables.js
+++ companion/variables.js
@@ -1,9 +1,6 @@
-// variables.js
-
 module.exports = function (self) {
-	// 6 adede kadar layer varsayalım, istersen daha da artırabilirsin
 	const variables = []
-	for (let i = 1; i <= 6; i++) {
+	for (let i = 1; i <= 8; i++) {
 		variables.push({ variableId: `layer_name_${i}`, name: `L${i} Layer Adı` })
 	}
 	self.setVariableDefinitions(variables)
