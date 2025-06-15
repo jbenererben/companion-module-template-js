9b4c3d5cd7ad5678bc06d360da9b28325307043c
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-15 21:04:36 +0300

    Update variables.js

--- companion/variables.js
+++ companion/variables.js
@@ -1,7 +1,34 @@
 module.exports = function (self) {
 	const variables = []
+	
+	// 8 adet layer adı variable'ı tanımla
 	for (let i = 1; i <= 8; i++) {
-		variables.push({ variableId: `layer_name_${i}`, name: `L${i} Layer Adı` })
+		variables.push({ 
+			variableId: `layer_name_${i}`, 
+			name: `L${i} Layer Adı`,
+			defaultValue: `L${i}` // Varsayılan değer ekle
+		})
 	}
+	
+	// Ek bilgi variable'ları
+	variables.push({
+		variableId: 'selected_screen_id',
+		name: 'Seçili Screen ID',
+		defaultValue: '1'
+	})
+	
+	variables.push({
+		variableId: 'selected_layer_id',
+		name: 'Seçili Layer ID',
+		defaultValue: ''
+	})
+	
 	self.setVariableDefinitions(variables)
+	
+	// Varsayılan değerleri set et
+	for (let i = 1; i <= 8; i++) {
+		self.setVariableValue(`layer_name_${i}`, `L${i}`)
+	}
+	self.setVariableValue('selected_screen_id', '1')
+	self.setVariableValue('selected_layer_id', '')
 }
